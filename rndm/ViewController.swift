8436260ed43678437915b7b2521dd5037486fc14
Thomas Jack Pickup <tjpickup@me.com>
2018-11-25 22:08:30 +0000

    Initial Commit

---
+++ rndm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rndm
+//
+//  Created by Thomas Pickup on 25/11/2018.
+//  Copyright © 2018 reviseolog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
